945291de4fa79c038e287a81cb0da7628bce646e
Egor <chuvak.postal.84@mail.ru>
2023-02-11 23:57:31 +0300

    Дз на болешее или меньшее число

---
+++ Задача2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа 
+//и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число");
+string example = Console.ReadLine();
+int a = Convert.ToInt32(example);
+Console.WriteLine("Введите второе число");
+string example1 = Console.ReadLine();
+int b = Convert.ToInt32(example1);
+if (a > b)
+{
+    int max = a;
+    int min = b;
+    Console.Write(max);
+    Console.Write(" больше чем ");
+    Console.WriteLine(min);
+}
+else
+{
+    int max = b;
+    int min = a;
+    Console.Write(max);
+    Console.Write(" больше чем ");
+    Console.WriteLine(min);
+}
